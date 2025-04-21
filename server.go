432a22fd616e78a305711ce8bac9e2c1c21b9892
Mike Rybka <merybka@gmail.com>
2025-04-21 09:56:27 -0400

    Create reverseproxy.Server

---
+++ server.go
@@ -0,0 +1,25 @@
+package reverseproxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+type Server struct {
+	Routes   map[string]string
+	NotFound http.Handler
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	for k, v := range s.Routes {
+		u, err := url.Parse(v)
+		if err != nil {
+			panic(err)
+		}
+		http.Handle(k, httputil.NewSingleHostReverseProxy(u))
+	}
+	mux.Handle("/", s.NotFound)
+	mux.ServeHTTP(w, r)
+}
